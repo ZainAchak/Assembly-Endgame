276eeb3d3eb2293264faabd88fdbbdadba875bde
Zain Ullah <zainullahachakzai@gmail.com>
2025-07-07 15:56:30 +0500

    optimizing Code

---
+++ src/components/LanguagesDescription.js
@@ -0,0 +1,68 @@
+export const languagesDescription = [
+  {
+    id: crypto.randomUUID(),
+    value: "HTML, it's been real.",
+    color: "#e34c26",
+    die: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    value: "CSS, I guess you just couldn't style your way out of this.",
+    color: "#264de4",
+    die: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    value: "JavaScript, too dynamic for your own good.",
+    color: "#d4c132",
+    die: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    value: "React, even hooks couldn’t save you.",
+    color: "#61dafb",
+    die: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    value: "TypeScript, type-safe but not safe enough.",
+    color: "#3178c6",
+    die: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    value: "Node.js, no more event loops for you.",
+    color: "#68a063",
+    die: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    value: "Python, your simplicity met a complex end.",
+    color: "#3572A5",
+    die: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    value: "Ruby, elegant to the last exception.",
+    color: "#cc342d",
+    die: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    value: "Assembly, Will RULE now.",
+    color: "#6e4c13",
+    die: false,
+  }
+];
+
+export const languagesPattren = [
+  { id: crypto.randomUUID(), value: "HTML", color: "#e34c26", show:true},
+  { id: crypto.randomUUID(), value: "CSS", color: "#264de4", show:true},
+  { id: crypto.randomUUID(), value: "Javascript", color: "#d4c132", show:true},
+  { id: crypto.randomUUID(), value: "React", color: "#61dafb", show:true},
+  { id: crypto.randomUUID(), value: "Typescript", color: "#3178c6", show:true},
+  { id: crypto.randomUUID(), value: "Node.js", color: "#68a063", show:true},
+  { id: crypto.randomUUID(), value: "Python", color: "#3572A5", show:true},
+  { id: crypto.randomUUID(), value: "Ruby", color: "#cc342d", show:true},
+  { id: crypto.randomUUID(), value: "Assembly", color: "#6e4c13", show:true}
+];
